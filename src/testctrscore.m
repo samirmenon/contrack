0c0cb9fee8fbe01ba62a9e7a3c1e3cff629551cc
Samir <smenon@stanford.edu>
2013-02-16 00:59:41 -0800

    Updated wrt the other changes. Also including the default params for contrack_score

--- src/testctrscore.m
+++ src/testctrscore.m
@@ -9,6 +9,31 @@
 % Author : SM <smenon@stanford.edu>
 % Date   : 2013-02-08
 
+% Parameters from the standard run:
+% 
+% Params: 1
+% Image Directory: /home/samir/Code/fmri/contrack.git/data/sub100311rd/dti06trilin/bin/ 
+image_dir = '/home/samir/Code/fmri/contrack.git/data/sub100311rd/dti06trilin/bin/';
+% WM/GM Mask Filename: wmProb.nii.gz
+wmgm_mask = 'wmProb.nii.gz';
+% PDF Filename: pdf.nii.gz
+pdf_file = 'pdf.nii.gz';
+% ROI MASK Filename: ltLGN_ltCalcFreesurfer_9_20110827T152126.nii.gz
+roi_file = 'ltLGN_ltCalcFreesurfer_9_20110827T152126.nii.gz';
+% Desired Samples: 10000
+% Max Pathway Nodes: 240
+% Min Pathway Nodes: 3
+% Step Size (mm): 1
+% Start Is Seed VOI: true
+% End Is Seed VOI: true
+% Save Out Spacing: 50
+% Threshold for WM/GM specification: 0.01
+% Absorption Rate WM: 0
+% Absorption Rate NotWM: 0
+% Local Path Segment Smoothness Standard Deviation: 14
+% Local Path Segment Angle Cutoff: 130
+% ShapeFunc Params (LinMidCl,LinWidthCl,UniformS): [ 0.175, 0.15, 100 ]
+
 clear;
 
 %% Load fibers : Note that these are typically in ras xyz real-world coordinates.
@@ -16,18 +41,20 @@ fiber_file = '/home/samir/Code/fmri/contrack.git/data/sub100311rd/fibers/conTrac
 fg = dtiLoadFiberGroup(fiber_file);
 
 %% Plot the fibers for a single fiber to make sure everything looks good.
-fib_id = 2;
+fib_id = 200;
 figure(1);
 plot3(fg.fibers{fib_id}(1,:),fg.fibers{fib_id}(2,:), fg.fibers{fib_id}(3,:)'.'); hold on;
 
 %% Load the diffusion tensors
 file_tensor = '/home/samir/Code/fmri/contrack.git/data/sub100311rd/dti06trilin/bin/tensors.nii.gz';
-dwiData = load_nifti(file_tensor);
-
-% Get tensors for a path
-tensors = ctrExtractDWITensorsAlongPath(fg.fibers{fib_id},dwiData);
+dwiData = load_nifti(file_tensor); % Just to get the xform
+fib2voxXform = inv(dwiData.vox2ras);
+[dt6, xformToAcpc, mmPerVoxel, fileName, desc, intentName] = dtiLoadTensorsFromNifti(file_tensor);
 
 %% Plot the diffusion tensors for the path (just to see that all is well)
+% Get tensors for a path
+tensors = ctrExtractDWITensorsAlongPath(fg.fibers{fib_id}, dt6, fib2voxXform);
+
 for i=1:size(fg.fibers{fib_id},2),
   % now plot the rotated ellipse
   [x, y, z] = ctrPlotGetPointSamplesOnEllipsoid( ...
@@ -40,6 +67,6 @@ figure(1); hold off; grid on; axis equal; axis tight;
 
 %% Now score the path.
 tmpStructural = dwiData.vol(:,:,:,1,1);
-scores = contrack_score(fg, dwiData, tmpStructural,  tmpStructural.*0 + 1);
+scores = contrack_score(fg, dt6, fib2voxXform, tmpStructural,  tmpStructural.*0 + 1);
 
 
