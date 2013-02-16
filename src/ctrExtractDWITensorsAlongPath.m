a7f923bdb4e6d08ee59d84fa13de8fbb1eb4915c
Samir <smenon@stanford.edu>
2013-02-16 00:59:27 -0800

    Remove ras references

--- src/ctrExtractDWITensorsAlongPath.m
+++ src/ctrExtractDWITensorsAlongPath.m
@@ -1,26 +1,19 @@
-function [ tensors ] = ctrExtractDWITensorsAlongPath(xyzTractRAS, dwiData )
+function [ tensors ] = ctrExtractDWITensorsAlongPath(xyzTract, dt6, fib2voxXform )
 %CTREXTRACTDWITENSORSALONGPATH Extracts the tensors along a set of fiber
 %paths
 %   Extracts diffusion tensors along a set of pathways
 
 tensors = [];
 
-% Get vox positions for tensor x,y,z,1,6
-% Remove the 1
-dwiTensors = squeeze(dwiData.vol); 
-
-%The fibers are in xyz. So convert the DWI data from voxels to xyz.
-ras2vox = inv(dwiData.vox2ras); 
-
 % Convert the tract's xyz coordinates (ras?) into voxel coordinates
-fibXYZRAS = xyzTractRAS;
-fibXYZAff = [fibXYZRAS;ones(1,size(fibXYZRAS,2))];
-fibXYZVox = ras2vox * fibXYZAff;
+fibXYZ = xyzTract;
+fibXYZAff = [fibXYZ;ones(1,size(fibXYZ,2))];
+fibXYZVox = fib2voxXform * fibXYZAff;
 fibXYZVoxNN = round(fibXYZVox);
 
 % Find the tensors along the tract's voxels
 for j=1:size(fibXYZVoxNN,2),
-  D = dwiData.vol(fibXYZVoxNN(1,j),fibXYZVoxNN(2,j),fibXYZVoxNN(3,j),1,:);
+  D = dt6(fibXYZVoxNN(1,j),fibXYZVoxNN(2,j),fibXYZVoxNN(3,j),:);
   D = squeeze(D);
   D = [D(1), D(4), D(5);
        D(4), D(2), D(6);
