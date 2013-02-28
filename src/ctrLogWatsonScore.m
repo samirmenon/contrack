e0d5d66ea6b6359599b6549c85ea5bca6fed748a
Samir <smenon@stanford.edu>
2013-02-27 16:48:04 -0800

    Adding log scoring functions

---
+++ src/ctrLogWatsonScore.m
@@ -0,0 +1,22 @@
+function [ logWatScore ] = ctrLogWatsonScore(CW, sigmaC, thetaSeg)
+%CTWatsonSCORE Computes the Watson score at a point on a fiber tract
+%  NOTE TODO : Replace cryptic argument names with descriptive ones.
+%  Arguments:
+%   CW: C(σ). The normalizing constant that ensures the Bingman
+%      distribution integrates to 1 over the unit sphere at this point.
+%   sigmaC: Angular Dispersion parameter for the dataset
+%   thetaSeg: Angle between two segment chunks
+%  
+%  Returns:
+%    watScore: C(σ) exp(cos(thetaSeg)^2 / sin(σ)^2)
+% logwatScore: C(σ) + cos(thetaSeg)^2 / sin(σ)^2
+
+% Requires a curve of 90 degrees or less.
+if abs(thetaSeg) > pi/2,
+  logWatScore = log(0);
+else
+  logWatScore = log(CW) + cos(thetaSeg)^2 / sin(sigmaC)^2;
+end
+
+end
+
