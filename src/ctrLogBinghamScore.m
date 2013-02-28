13abc9d6c7a0099384e733471f76e77f83ed23ee
Samir <smenon@stanford.edu>
2013-02-27 16:48:04 -0800

    Adding log scoring functions

---
+++ src/ctrLogBinghamScore.m
@@ -0,0 +1,42 @@
+function [ logBhamScore ] = ctrLogBinghamScore(t, D, C, CL, sigmaM, eta )
+%CTRLOGBINGHAMSCORE Computes the log Bingham score at a point on a fiber tract
+%  NOTE TODO : Replace cryptic argument names with descriptive ones.
+%  Arguments:
+%   t: The tangent to the path at this point.
+%   D: The diffusion tensor at the point along a tract
+%   C: C(σ3, σ2). The normalizing constant that ensures the Bingman
+%      distribution integrates to 1 over the unit sphere at this point.
+%   CL: Tensor linearity index
+%   sigmaM: Dispersion parameter for the dataset
+%   eta: User parameter that modulates the dispersion factor.
+%  
+%  Returns:
+%    bhamScore: C(σ3, σ2) exp(-(v3'*t / sin(σ3))^2 -(v2'*t / sin(σ2))^2)
+% logbhamScore: log(C(σ3, σ2))  - (v3'*t / sin(σ3))^2 - (v2'*t / sin(σ2))^2 
+%               σi = σm + σi* 
+%               σi* = λi' / (λi + λi') * δ
+%               δ = 100deg / ( 1 + exp(- (η - CL) / 0.015) )
+
+% Compute the eigenvectors and eigenvalues of the diffusion tensor
+[v d] = eigs(D);
+
+% Compute delta = 100deg / (1+ exp( - (eta - CL) / 0.015 ) );
+% SM : Update. We move to radians instead of degrees. (Check this later)
+delta = (100 * pi/180) / ( 1 + exp(- (eta - CL) / 0.015) );
+
+% Compute the term for eigenvector 3
+sigma3star = d(3,3) / ( d(2,2) + d(3,3) ) * delta;
+sigma3 = sigmaM + sigma3star;
+t3 = v(:,3)'*t;
+t3 = t3 / sin(sigma3);
+
+% Compute the term for eigenvector 2
+sigma2star = d(2,2) / ( d(2,2) + d(3,3) ) * delta;
+sigma2 = sigmaM + sigma2star;
+t2 = v(:,2)'*t;
+t2 = t2 / sin(sigma2);
+
+logBhamScore = log(C) - (t3*t3) - (t2*t2);
+
+end
+
