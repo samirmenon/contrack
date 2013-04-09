3c039ee9344646d860aa91258452b1c17f49a68f
Samir <smenon@stanford.edu>
2013-04-08 17:47:29 -0700

    Updating the sphere integrator

--- src/ctrBinghamSphereIntegrator.m
+++ src/ctrBinghamSphereIntegrator.m
@@ -1,4 +1,4 @@
-function [ output_args ] = ctrBinghamSphereIntegrator( D, thermalNoiseK, uniformityParam, linearityMidCl, linearityWidthCl, CL)
+function [ localLogLikelihood ] = ctrBinghamSphereIntegrator( D, thermalNoiseK, uniformityParam, linearityMidCl, linearityWidthCl, CL)
 %CTRBINGHAMSPHEREINTEGRATOR This function computes the normalizing constant
 %required for sampling a Bingham distribution over a sphere.
 %  Arguments:
@@ -9,6 +9,9 @@ function [ output_args ] = ctrBinghamSphereIntegrator( D, thermalNoiseK, uniform
 %     linearityMidCl : η in the paper
 %   linearityWidthCl : 0.015 in the paper
 %                 CL : Tensor linearity index
+% 
+% HISTORY:
+% 2013.03 SM: wrote it.
 
 %Hard wired constant in cpp code
 if( thermalNoiseK > 205 || thermalNoiseK == 0)
@@ -47,8 +50,59 @@ if(k2 > -1 ) k2 = -1; end;
 if( k2 < -205 || k1 < -205 ) k2 = -205; k1 = -205; end;
 if( k1 > k2 ) k1 = k2 - 0.01; end; %// This has only happened because of round off error
 
-localLogLikelihood = computeLocalLogLikelihoodFB5(tangent,p.vE1,p.vE2,k1,k2,angle,deviation);
-        
-        
+localLogLikelihood = computeLocalLogLikelihoodFB5(tangent,p.vE1,p.vE2,k1,k2);        
 end
 
+% ************************************************************************
+% x = tangent vector. e1 = eig 1. e2 = eig 2. k1 and k2 are some weird
+% scanner noise constants.
+function [logL, angle, deviation] = computeLocalLogLikelihoodFB5(x, e1, e2, k1, k2)
+cosTheta = x. e1';
+deviation = k1;
+
+if(cosTheta > 1)
+  angle = 0;
+elseif( cosTheta < -1)
+  angle = pi;
+else
+  angle = acos(cosTheta);
+end
+logL= logFB5PDF(x,e1,e2,k1,k2) - 2.53; % log(1/(4*PI))
+end
+
+% ************************************************************************
+% 
+function [ logL ] = logFB5PDF(x, e1, e2, k1, k2)
+% Find normalization
+log_A = log( normFB5(k1, k2) );
+
+logL = logFB5PDF(x, e1, e2, k1, k2, log_A);
+end
+
+% ************************************************************************
+%
+function [ A ] = normFB5(k1, k2)
+A = 0;
+if (k2 > 0.0 || k1 > k2)
+  tmp_errmsg = sprintf('llegal concentration parameters {k1, k2} = { %f, %f}',k1,k2);
+  error(tmp_errmsg);
+  return;
+end
+der = zeros(2,1);
+hes = zeros(2,1);
+level = 8;
+
+%// from Kent's paper
+if (k2 < -8.5)
+  mode = 3;
+else
+  mode = 1;
+  if (k1 <= -10.0 && k1 / k2 >= 2.0)
+    mode = 2;
+  end
+end
+A = normFB5CleanMeUpAndDeleteMe(k1, k2, der, hes, mode, level) / (4.0 * pi);
+end
+
+% ************************************************************************
+%
