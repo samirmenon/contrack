7728e65a22735404e306661212524f97ed6052eb
Samir <smenon@stanford.edu>
2014-11-12 22:24:06 -0800

    Updating the plotter test code

--- scripts/testBhamMath.m
+++ scripts/testBhamMath.m
@@ -8,7 +8,7 @@ clear;
 addpath([pwd '/../src']);
 
 if ~exist('B','var'),  
-  B = load('../data/bhamConst-MixEigs-0.002.txt');
+  B = load('../data/bhamConst-0.002.txt');
   % Invert bham integral to convert it into a normalizing constt...
   B(:,4) = 1./B(:,4);
 end
@@ -32,12 +32,20 @@ axis tight;
 axis square;
 grid on;
 
-figname = ['./figs/Fig_BhamPartFunc.svg'];
-plot2svg(figname);
+figname = ['./figs/Fig_BhamPartFunc'];
+
+if(2 == exist('plot2svg','file'))
+  plot2svg([figname '.svg']);
+end
+if(2==exist('export_fig','file'))
+  export_fig ([figname '.png'], '-png');
+end
+
+
 
 %% Perfect contrack scenario. Simulated data. No distortion...
-
-eigset = {[1 .5 .5], [1 .5 .1], [3 1.2 0.1]};
+close all;
+eigset = {[1 1 .001], [1 .1 .1], [1 .5 .1], [1 .5 .5], [1 1 .1], [1 1 .5], [1 1 1]};
 for iii = 1:1:length(eigset),
   j=1;
   tensors = []; valid = [];
@@ -71,95 +79,48 @@ for iii = 1:1:length(eigset),
   
   for i=1:1:length(samples),
     tau=samples(i);
-    t = [(1-tau) tau 0.001];
+    t = [(1-tau) tau 0];
     t = t./norm(t);
     
     D = tensors{j}.D;
     C = tensors{j}.C;
     
-    % **************** Actual Bham Computation Code **********************
-    % If t is a row vector resize it to be a col vector
-    if min(size(t) == [1 3]),
-      t = t';
-    end
-    
-    sigmaM = 0.2443; % == 14 deg..pi*14/180 rad; User param. From paper (pg. 7 col. 2, para 1)
-    eta = .175; % User param. From paper (pg. 7 col. 2, para 2)
-    
-    % D is a cell array, assume that it is {eigvals, eigvecs, trace}:
-    v = D{1};
-    d = D{2};
-    
-    % CL: Tensor linearity index = abs(eig1 - eig2) / sum(eigs);
-    % Right-2nd para on page 4 of Contrack J'Vision 2008.
-    % "The linearity index is a measure of anisotropy, and is the positive
-    % difference between the largest two eigenvalues of the diffusion tensor
-    % divided by the sum of its eigenvalues"
-    %    Value range : [0, 1)
-    CL = abs(d(1) - d(2)) / D{3};
-    
-    % Compute delta = 100deg / (1+ exp( - (eta - CL) / 0.015 ) );
-    % SM : We will move to radians instead of degrees. Original cpp is not
-    % implemented with SI units (???).
-    % 100 deg =  pi*100/180 rad = 1.7453 rad
-    %
-    %  NOTE : Discrepancy wrt. cpp file code:
-    % double linshape_ds = uniform_s / (1+exp(-(linearityMidCl-p.fCl)*10/linearityWidthCl));
-    % linearityMidCl = 0.174;  linearityWidthCl = .174 (differs from paper)
-    % However, both codes give similar delta values in the end...
-    %
-    % Value range : CL=0,delta=100; CL=.1.8,delta=42; CL=0.3,delta=0ish...
-    delta = 1.7453 / ( 1 + exp(- (eta - CL) / 0.015) ); % 100 in degrees
-    
-    % Compute the term for eigenvector 3
-    sigma3star = d(3) / ( d(2) + d(3) ) * delta;
-    sigma3 = sigmaM + sigma3star;
-    t3 = v(:,3)'*t;
-    t3 = t3 / sin(sigma3);
-    
-    % Compute the term for eigenvector 2
-    sigma2star = d(2) / ( d(2) + d(3) ) * delta;
-    sigma2 = sigmaM + sigma2star;
-    t2 = v(:,2)'*t;
-    t2 = t2 / sin(sigma2);
-    
-    % To understand the score, consider some cases:
-    %
-    % Case 1: Perfectly aligned with top eigenvector.
-    %  t = e0, bhamscore = C * e(0) = C
-    %
-    % Case 2: Perfectly aligned with second eigvec, 2nd,3rd eigval are .5.
-    %  t = e1, bhamscore = C * 1/sin(14deg + [0,100)deg)
-    %
-    bhamScore = C * exp( -(t3*t3) - (t2*t2));
-    % **************** END : Actual Bham Computation Code **********************
-    
-    % Convert Bham score to probability...
-    % Polar coords... In case you want the exact probability within a certain
-    % nearby region (say +-5 degrees or something; area along both dirs)
-    r = 1; %t must be a unit vector..
-    angle_region = 5 * pi/180; %for +-5 deg
-    h = 1 - cos(angle_region);
-    dar = 2*pi*r*h;
-    
-    sco(i) = bhamScore * dar;
+    % ******************* COMPUTE THE BINGHAM SCORE **********************
+    sco(i) = ctrBinghamScore(t, D, C);
+    % ****************** COMPUTED THE BINGHAM SCORE **********************
   end
   
-  figure; hold off;
+  fignum = figure('XVisual',...
+    '0x27 (TrueColor, depth 24, RGB mask 0xff0000 0xff00 0x00ff)',...
+    'Color',[1 1 1]);
+  hold off;
   plot(samples,sco,'k','LineWidth',2);
   box off;
-  xlabel('tau: fiber dir = (1-tau, tau, 0.001)./norm');
+  xlabel('Fiber direction.');
+  set(gca, ...
+  'XTick', [0 0.5 1], ...
+  'XTickLabels', {'[1 0 0]', '[0.7071 0.7071 0]', '[0 1 0]'});
   ylabel('Bingham Score');
-  title(sprintf('Score changes for tensor eigs: %.3f, %.3f, %.3f',d(1), d(2), d(3)));
+  title(sprintf('Bingham Score for Tensor Eigs (along x,y,z): %.3f, %.3f, %.3f',d(1), d(2), d(3)));
   
-  figname = sprintf('./figs/Fig_BhamFuncVal_%.3f_%.3f_%.3f.svg',d(1), d(2), d(3));
-  plot2svg(figname);
+  figname = sprintf('./figs/Fig_BhamFuncVal_%.3f_%.3f_%.3f',d(1), d(2), d(3));
+  if(2 == exist('plot2svg','file'))
+    plot2svg([figname '.svg']);
+  end
+  if(2==exist('export_fig','file'))
+    export_fig ([figname '.png'], '-png');
+  end
 end
 
 %% Plot some interesting aspects of the algorithm.
 % Compare the dispersion factor spec in the Contrack Paper and C++ code..
-figure;
+fignum = figure('XVisual',...
+  '0x27 (TrueColor, depth 24, RGB mask 0xff0000 0xff00 0x00ff)',...
+  'Color',[1 1 1]);
+
 i=1; sco = []; 
+sigmaM = 0.2443; % == 14 deg..pi*14/180 rad; User param. From paper (pg. 7 col. 2, para 1)
+eta = .175; % User param. From paper (pg. 7 col. 2, para 2)
 for CL = 0:0.01:1, 
   sco(i) = (pi*100/180) / ( 1 + exp(- (eta - CL) / 0.015) );i=i+1; 
 end;  
@@ -182,5 +143,10 @@ xlabel('CL [ Tensor linearity index = eig(1)-eig(2) / sum(eigs) ]');
 ylabel('delta [Dispersion factor]');
 title('Delta computed as per C++ code');
 
-figname = sprintf('./figs/Fig_TensorLin.svg');
-plot2svg(figname);
+figname = sprintf('./figs/Fig_TensorLin');
+if(2 == exist('plot2svg','file'))
+  plot2svg([figname '.svg']);
+end
+if(2==exist('export_fig','file'))
+  export_fig ([figname '.png'], '-png');
+end
